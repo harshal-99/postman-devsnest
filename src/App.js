6e354b946f73db6f1fb1a310f689a3d454bcc9fa
Harshal Pawshekar <harshalpawshekar999@gmail.com>
2023-01-07 20:04:06 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import Select from "@mui/material/Select";
+import InputLabel from "@mui/material/InputLabel";
+import {useState} from "react";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const requestType = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'];
+	const [request, setRequest] = useState(requestType[0]);
+	const [url, setUrl] = useState('');
+	const [response, setResponse] = useState('');
+
+	function handleSelectChange(event) {
+		setRequest(event.target.value);
+	}
+
+	async function sendRequest() {
+		console.log(request, url);
+		let response
+		switch (request) {
+			case 'GET': {
+				response = await axios.get(url);
+				break;
+			}
+			/*			case 'POST': {
+							response = await axios.post(url);
+						}*/
+		}
+		setResponse(response.data);
+		console.log(response.data);
+	}
+
+	return (
+		<div className="flex flex-col justify-center items-center h-screen">
+			<div className="flex justify-around w-full">
+				<FormControl variant="standard">
+					<InputLabel id="type">Type</InputLabel>
+					<Select
+						labelId='type'
+						id='type'
+						value={request}
+						label='Type'
+						onChange={handleSelectChange}
+					>
+						{requestType.map(type => <MenuItem key={type} value={type}>{type}</MenuItem>)}
+					</Select>
+				</FormControl>
+				<TextField className="w-3/5" id="url" label='URL' variant="outlined"
+				           value={url} onChange={e => setUrl(e.target.value)}
+				/>
+				<Button variant="contained" onClick={sendRequest}>Send</Button>
+			</div>
+			<div>
+				Configs
+			</div>
+			<div>
+				{JSON.stringify(response)}
+			</div>
+		</div>
+	);
 }
 
 export default App;
